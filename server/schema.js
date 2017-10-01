0289c3a8cde48169027e5b18aea967b78c420448
Daryl Carr <darylcarr@live.co.uk>
2017-10-01 18:29:25 +0100

    Add graphql query for all articles and articlesByTopic

--- server/schema.js
+++ server/schema.js
@@ -2,10 +2,12 @@ const {
     GraphQLSchema,
     GraphQLObjectType,
     GraphQLList,
-    GraphQLString
+    GraphQLString,
+    GraphQLInt
 } = require('graphql');
 
 const Topic = require('./models/topics');
+const Article = require('./models/articles');
 
 const TopicType = new GraphQLObjectType({
     name: 'Topic',
@@ -26,14 +28,57 @@ const TopicType = new GraphQLObjectType({
     })
 });
 
+const ArticleType = new GraphQLObjectType({
+    name: 'Article',
+    description: '...',
+    fields: () => ({
+        _id: {
+            type: GraphQLString,
+            resolve: topic => topic._id
+        },
+        title: {
+            type: GraphQLString,
+            resolve: topic => topic.title
+        },
+        body: {
+            type: GraphQLString,
+            resolve: topic => topic.body
+        },
+        belongs_to: {
+            type: GraphQLString,
+            resolve: topic => topic.belongs_to
+        },
+        votes: {
+            type: GraphQLInt,
+            resolve: topic => topic.votes
+        },
+        created_by: {
+            type: GraphQLString,
+            resolve: topic => topic.created_by
+        }
+    })
+});
+
 module.exports = new GraphQLSchema({
     query: new GraphQLObjectType({
         name: 'Query',
         description: '...',
         fields: () => ({
-            topics: {
+            allTopics: {
                 type: new GraphQLList(TopicType),
                 resolve: () => Topic.find()
+            },
+            allArticles: {
+                type: new GraphQLList(ArticleType),
+                resolve: () => Article.find()
+            },
+            articlesByTopic: {
+                type: new GraphQLList(ArticleType),
+                args: {
+                    topicName: {type: GraphQLString}
+                },
+                resolve: (root, args) => 
+                    Article.find({belongs_to: args.topicName})
             }
         })
     })

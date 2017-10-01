6dd8a335c8ed34f5b69514a53568768215603f78
Daryl Carr <darylcarr@live.co.uk>
2017-10-01 16:52:02 +0100

    Apply graphQL middleware

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = require('./server');
-const PORT = require('./config').PORT[process.env.NODE_ENV];
+const app = require('./server/server');
+const PORT = require('./config').PORT;
 
 app.listen(PORT, function () {
   console.log(`listening on port ${PORT}`);
--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
 if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
 
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var app = express();
-var config = require('./config');
-var db = config.DB[process.env.NODE_ENV] || process.env.DB;
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHTTP = require('express-graphql');
+
+const mongoose = require('mongoose');
 mongoose.Promise = Promise;
+
+const config = require('../config');
+const schema = require('./schema');
+const db = process.env.DB || config.DB;
+const env= process.env.NODE_ENV || 'dev';
+
+const app = express();
 
 mongoose.connect(db, {useMongoClient: true})
   .then(() => console.log('successfully connected to', db))
   .catch(err => console.log('connection failed', err));
 
 app.use(bodyParser.json());
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: env === 'dev'
+}));
 
 module.exports = app;
